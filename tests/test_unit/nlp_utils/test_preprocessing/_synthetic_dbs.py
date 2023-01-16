a54989c583e73a8a01a76279629d4f06c8f11691
Amin Ghadesi <amin.ghadesi@gmail.com>
2023-01-16 08:40:54 -0500

    Renamed a twitter db.

--- tests/test_unit/nlp_utils/test_preprocessing/_synthetic_dbs.py
+++ tests/test_unit/nlp_utils/test_preprocessing/_synthetic_dbs.py
@@ -39,3 +39,27 @@ class Synthetic_tweet_emotion_en_db:
 
     def get_row_info(self, text: str) -> pd.DataFrame:
         return self.selected_tweet_df.loc[self.selected_tweet_df['Texr'] == text]
+
+
+class Synthetic_tweet_emotion_en_db:
+    def __init__(self, n_samples: int = 100, seed: int = 100):
+        self.n_samples = n_samples
+        self.seed = seed
+
+        tweet_df = pd.read_csv("./Datasets/Twitter_DS_emotion.txt", sep="\t", header=None, names=["Index", "Text", "Feeling"])
+        self.selected_tweet_df = tweet_df.sample(n=n_samples, random_state=seed)
+
+    def __repr__(self) -> str:
+        return f"Synthetic_tweet_emotion_en_db(n_samples={self.n_samples}, seed={self.seed})"
+
+    def get_list(self) -> List:
+        return self.selected_tweet_df.Text.values.tolist()
+
+    def get_df(self) -> pd.DataFrame:
+        return self.selected_tweet_df
+
+    def get_label(self) -> List:
+        return self.selected_tweet_df.Feeling.values.tolist()
+
+    def get_row_info(self, text: str) -> pd.DataFrame:
+        return self.selected_tweet_df.loc[self.selected_tweet_df['Texr'] == text]
