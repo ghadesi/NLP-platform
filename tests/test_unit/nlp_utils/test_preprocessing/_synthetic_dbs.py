23d481604bdd1b3a9b9179fe5356b12da81fcfd8
Amin Ghadesi <amin.ghadesi@gmail.com>
2023-01-16 08:36:59 -0500

    Added the __repr__ function.

--- tests/test_unit/nlp_utils/test_preprocessing/_synthetic_dbs.py
+++ tests/test_unit/nlp_utils/test_preprocessing/_synthetic_dbs.py
@@ -16,19 +16,25 @@ from typing import List
 
 # ───────────────────────────────── DBs Class ────────────────────────────────── #
 
+
 class Synthetic_tweet_emotion_en_db:
     def __init__(self, n_samples: int = 100, seed: int = 100):
+        self.n_samples = n_samples
+        self.seed = seed
         tweet_df = pd.read_csv("./Datasets/Twitter_DS.txt", sep="\t", header=None, names=["Index", "Text", "Feeling"])
         self.selected_tweet_df = tweet_df.sample(n=n_samples, random_state=seed).Text.values.tolist()
-        
+
+    def __repr__(self) -> str:
+        return f"Synthetic_tweet_emotion_en_db(n_samples={self.n_samples}, seed={self.seed})"
+
     def get_list(self) -> List:
         return self.selected_tweet_df.Text.values.tolist()
-    
+
     def get_df(self) -> pd.DataFrame:
         return self.selected_tweet_df
-    
+
     def get_label(self) -> List:
         return self.selected_tweet_df.Feeling.values.tolist()
-    
+
     def get_row_info(self, text: str) -> pd.DataFrame:
         return self.selected_tweet_df.loc[self.selected_tweet_df['Texr'] == text]
